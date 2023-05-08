51b457ec249108ea0361a9c2ef7879ed2598ae23
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-05-07 20:18:25 -0700

    sete up webpack and createed basic project structure.

--- README.md
+++ README.md
@@ -13,3 +13,13 @@ Display the information on your webpage!
 Add any styling you like!
 
 Optional: add a ‘loading’ component that displays from the time the form is submitted until the information comes back from the API.
+
+
+intialize npm
+install loaders for
+    CSS
+    Bootstrap
+    SCSS
+    JQuery
+    babel
+    
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        // ES6 Loader
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      // CSS Loader
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      // Bootstrap CSS
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: () => [
+                require("autoprefixer")({
+                  overrideBrowserslist: ["last 2 versions", "ie >= 9"],
+                }),
+              ],
+            },
+          },
+          "sass-loader",
+        ],
+        include: /node_modules/,
+      },
+
+      // Bootstrap JavaScript
+      {
+        test: /bootstrap\/dist\/js\/umd\//,
+        use: "imports-loader?jQuery=jquery",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
