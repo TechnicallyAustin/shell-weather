4ad2b78666bd41cd37e97de68d2fa036f0a7f26f
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-05-16 16:18:25 -0700

    structuring main sections

--- src/page/content/mainContent.js
+++ src/page/content/mainContent.js
@@ -9,8 +9,16 @@ export function mainContent() {
         },
         location: function(){
             // uses weather search api to get location
+            const location = main.appendChild(document.createElement('input'));
+            location.setAttribute('class', 'location text-white');
+            location.textContent = 'Search for a location';
         },
-        weatherContainer: function(){}
+        weatherContainer: function(){
+            const weatherContainer = main.appendChild(document.createElement('div'));
+            weatherContainer.setAttribute('class', 'weather-container bg-light w-100 h-50');
+        }
     };
     elements.title();
+    elements.location();
+    elements.weatherContainer();
 };
